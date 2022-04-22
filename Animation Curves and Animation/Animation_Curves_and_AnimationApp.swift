d71a9419dc076db059a68a41c3228c50f39e71ea
Oncu <oohancan@gmail.com>
2022-04-22 21:54:50 +0300

    Initial Commit

---
+++ Animation Curves and Animation/Animation_Curves_and_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_Curves_and_AnimationApp.swift
+//  Animation Curves and Animation
+//
+//  Created by Oncu Ohancan on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Animation_Curves_and_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
